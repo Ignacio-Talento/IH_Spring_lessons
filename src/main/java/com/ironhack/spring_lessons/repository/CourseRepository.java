c2181cb0618971487ef4f4751a1623764f86cc51
Ignacio Talento <ignaciotalento@gmail.com>
2025-05-07 11:00:09 +0200

    JPA custom queries created

--- src/main/java/com/ironhack/spring_lessons/repository/CourseRepository.java
+++ src/main/java/com/ironhack/spring_lessons/repository/CourseRepository.java
@@ -4,6 +4,13 @@ import com.ironhack.spring_lessons.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, String> {
+    Optional<Course> findByHours(Integer hours);
+    List<Course> findAllByClassroom(String classroom);
+    List<Course> findAllByCourseContaining(String str);
+    List<Course> findAllByHoursLessThan(Integer hours);
 }
